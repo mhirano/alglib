3e8e28454d6b462b5e432024b6fa5228d6ef0e6a
mhirano <Masahiro_Hirano@ipc.i.u-tokyo.ac.jp>
2019-08-01 11:12:06 +0900

    install on MacPro

--- v315/install/mac/share/cmake/alglib/alglib-config-debug.cmake
+++ v315/install/mac/share/cmake/alglib/alglib-config-debug.cmake
@@ -1,15 +1,15 @@
 #----------------------------------------------------------------
-# Generated CMake target import file for configuration "Release".
+# Generated CMake target import file for configuration "Debug".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "alglib" for configuration "Release"
-set_property(TARGET alglib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+# Import target "alglib" for configuration "Debug"
+set_property(TARGET alglib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(alglib PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libalglib.3.15.dylib"
-  IMPORTED_SONAME_RELEASE "@rpath/libalglib.3.15.dylib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libalglib.3.15.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libalglib.3.15.dylib"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS alglib )
