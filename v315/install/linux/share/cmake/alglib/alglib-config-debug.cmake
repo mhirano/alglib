e1eb74af279c2903f00ac5ae58e151647f609d05
mhirano <Masahiro_Hirano@ipc.i.u-tokyo.ac.jp>
2019-08-07 17:59:50 +0900

    install alglib in flash

---
+++ v315/install/linux/share/cmake/alglib/alglib-config-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "alglib" for configuration "Debug"
+set_property(TARGET alglib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(alglib PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libalglib.3.15d.so"
+  IMPORTED_SONAME_DEBUG "libalglib.3.15d.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS alglib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_alglib "${_IMPORT_PREFIX}/lib/libalglib.3.15d.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
